7016785c23f3275adac7e85d88f5c3c503ba86d0
Kevin Chua <Kevin_chua3@hotmail.com>
2023-11-30 13:15:59 +0000

    setting up 03-template inheritence

--- 03-template-inheritence/index.js
+++ 03-template-inheritence/index.js
@@ -1,5 +1,12 @@
 const express = require('express'); 
+const hbs = require('hbs'); // to configure hbs to use wax-on
+const wax = require('wax-on'); // wax-on is for template inheritance
+
 const app = express(); 
+
+// configure wax-on for tempate inheritance
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts")
 
 // ROUTES 
 app.get("/", function(req, res){
