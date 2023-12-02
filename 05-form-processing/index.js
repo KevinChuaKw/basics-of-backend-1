f8cfb5b9c0cfc32a9009963d96ac322461ef22fc
Kevin Chua <Kevin_chua3@hotmail.com>
2023-12-02 03:26:49 +0000

    setting up 05 form processing

--- 05-form-processing/index.js
+++ 05-form-processing/index.js
@@ -11,31 +11,10 @@ wax.setLayoutPath("./views/layouts") // a layout is a template that other hbs ca
 // use for HBS for templates
 app.set("view engine", "hbs"); 
 
-// writing a custom hbs helper to compare two values
-// 1st Parameter: the name of the helper
-// 2nd 
-hbs.handlebars.registerHelper("ifEquals", function(arg1, arg2, options){
-    if (arg1 == arg2){
-        // the 'this' refer to the hbs
-        return options.fn(this); 
-    } else {
-        return options.inverse(this); 
-    }
-}); 
-
 // ROUTES 
 app.get("/", function(req, res){
     res.render("index"); 
     }) 
-
-app.get("/", function (req,res){
-    const menu= ["Apple", "Watermellon", "Orange"];
-    const offer = true; 
-    res.render('index', {
-        fruits: menu
-        
-    })
-})
 
 app.get("/about-us", function(req, res){
     res.render("about"); 
